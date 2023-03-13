d61ddc4456fdd8a7b78b361d84fa8450032631a6
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-03-13 14:49:03 -0300

    Conclude first two javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let finalString = '';
+    if (num < 0) return "ERROR";
+
+    for (let i = 0; i<num;i++){
+        finalString +=string;
+    }
+    
+    return finalString;
 
 };
 
